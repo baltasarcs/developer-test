76f025ada3df93187ac186efaef386f3d24c0f0b
baltasarcs <baltasarc.s@gmail.com>
2015-07-14 20:42:54 -0300

    Update SintegraEs.php

--- php/spider/SintegraEs.php
+++ php/spider/SintegraEs.php
@@ -67,9 +67,11 @@ class SintegraEs {
 
 	function getIdentificacao($result) {
 
-		$patterns = array(	'cnpj'			=> '@<td\b[^>]*?>\s*CNPJ:</td>\s*<td\b[^>]*?>\s*(.*?)\s*</td>@is',
-							'ie'			=> '@<td\b[^>]*?>\s*Inscri.*?o\s*Estadual:</td><td\b[^>]*?>\s*(.*?)\s*</td>@is',
-							'razao_social'	=> '@<td\b[^>]*?>\s*Raz.*?o\s*Social\s*:</td><td\b[^>]*?>\s*(.*?)\s*</td>@is');
+		$patterns = array(	
+				'cnpj'		=> '@<td\b[^>]*?>\s*CNPJ:</td>\s*<td\b[^>]*?>\s*(.*?)\s*</td>@is',
+				'ie'		=> '@<td\b[^>]*?>\s*Inscri.*?o\s*Estadual:</td><td\b[^>]*?>\s*(.*?)\s*</td>@is',
+				'razao_social'	=> '@<td\b[^>]*?>\s*Raz.*?o\s*Social\s*:</td><td\b[^>]*?>\s*(.*?)\s*</td>@is'
+				);
 
 		foreach ($patterns as $c => $p) {
 
@@ -86,14 +88,16 @@ class SintegraEs {
 	
 	function getEndereco($result) {
 	
-		$patterns = array(	'logradouro'	=> '@<td\b[^>]*?>\s*Logradouro:</td>\s*<td\b[^>]*?>\s*(.*?)\s*</td>@i',
-							'numero'		=> '@<td\b[^>]*?>\s*N.*?mero:</td><td\b[^>]*?>\s*(.*?)\s*</td>@i',
-							'complemento'	=> '@<td\b[^>]*?>\s*Complemento:</td>\s*<td\b[^>]*?>\s*(.*?)\s*</td>@i',
-							'bairro'		=> '@<td\b[^>]*?>\s*Bairro:</td>\s*<td\b[^>]*?>\s*(.*?)\s*</td>@i',
-							'municipio'		=> '@<td\b[^>]*?>\s*Munic.*?pio:</td><td\b[^>]*?>(.*?)\s*</td>@i',
-							'uf'			=> '@<td\b[^>]*?>\s*UF:</td>\s*<td\b[^>]*?>\s*(.*?)\s*</td>@i',
-							'cep'			=> '@<td\b[^>]*?>\s*CEP:</td>\s*<td\b[^>]*?>\s*(.*?)\s*</td>@i',
-							'telefone'		=> '@<td\b[^>]*?>\s*Telefone:</td>\s*<td\b[^>]*?>\s*(.*?)\s*</td>@i'	);
+		$patterns = array(	
+				'logradouro'	=> '@<td\b[^>]*?>\s*Logradouro:</td>\s*<td\b[^>]*?>\s*(.*?)\s*</td>@i',
+				'numero'	=> '@<td\b[^>]*?>\s*N.*?mero:</td><td\b[^>]*?>\s*(.*?)\s*</td>@i',
+				'complemento'	=> '@<td\b[^>]*?>\s*Complemento:</td>\s*<td\b[^>]*?>\s*(.*?)\s*</td>@i',
+				'bairro'	=> '@<td\b[^>]*?>\s*Bairro:</td>\s*<td\b[^>]*?>\s*(.*?)\s*</td>@i',
+				'municipio'	=> '@<td\b[^>]*?>\s*Munic.*?pio:</td><td\b[^>]*?>(.*?)\s*</td>@i',
+				'uf'		=> '@<td\b[^>]*?>\s*UF:</td>\s*<td\b[^>]*?>\s*(.*?)\s*</td>@i',
+				'cep'		=> '@<td\b[^>]*?>\s*CEP:</td>\s*<td\b[^>]*?>\s*(.*?)\s*</td>@i',
+				'telefone'	=> '@<td\b[^>]*?>\s*Telefone:</td>\s*<td\b[^>]*?>\s*(.*?)\s*</td>@i'	
+				);
 
 		foreach ($patterns as $c => $p) {
 
@@ -110,14 +114,15 @@ class SintegraEs {
 	
 	function getComplemento($result) {
 
-		$patterns = array(	'atividade'				=> '@<td\b[^>]*?>\s*Atividade\s*Econ.*?mica\s*:\s*</td><td\b[^>]*?>\s*(.*?)\s*</td>@is',
-							'data_inicio'			=> '@<td\b[^>]*?>\s*Data\s*de\s*Inicio\s*de\s*Atividade\s*:\s*</td>\s*<td\b[^>]*?>\s*(.*?)\s*</td>@i',
-							'situacao_vigente'		=> '@<td\b[^>]*?>\s*Situa.{1,16}o\s*Cadastral\s*Vigente\s*:\s*</td><td\b[^>]*?>\s*(.*?)\s*</td>@is',
-							'data_situacao_vigente'	=> '@<td\b[^>]*?>\s*Data\s*desta\s*Situa.*?o\s*Cadastral\s*:\s*</td><td\b[^>]*?>\s*(.*?)\s*</td>@is',
-							'regime_apuracao'		=> '@<td\b[^>]*?>\s*Regime\s*de\s*Apura.{1,16}\s*:\s*</td>\s*<td\b[^>]*?>\s*(.*?)\s*</td>@i',
-							'emitente_nfe_desde'	=> '@<td\b[^>]*?>\s*Emitente\s*de\s*NFe\s*desde:\s*</td>\s*<td\b[^>]*?>\s*(.*?)\s*</td>@i',
-							'obrigada_nfe'			=> '@<td\b[^>]*?>\s*Obrigada\s*a\s*NF-e\s*em:\s*</td>\s*<td\b[^>]*?>\s*(.*?)\s*</td>@i'							
-						);
+		$patterns = array(	
+				'atividade'		=> '@<td\b[^>]*?>\s*Atividade\s*Econ.*?mica\s*:\s*</td><td\b[^>]*?>\s*(.*?)\s*</td>@is',
+				'data_inicio'		=> '@<td\b[^>]*?>\s*Data\s*de\s*Inicio\s*de\s*Atividade\s*:\s*</td>\s*<td\b[^>]*?>\s*(.*?)\s*</td>@i',
+				'situacao_vigente'	=> '@<td\b[^>]*?>\s*Situa.{1,16}o\s*Cadastral\s*Vigente\s*:\s*</td><td\b[^>]*?>\s*(.*?)\s*</td>@is',
+				'data_situacao_vigente'	=> '@<td\b[^>]*?>\s*Data\s*desta\s*Situa.*?o\s*Cadastral\s*:\s*</td><td\b[^>]*?>\s*(.*?)\s*</td>@is',
+				'regime_apuracao'	=> '@<td\b[^>]*?>\s*Regime\s*de\s*Apura.{1,16}\s*:\s*</td>\s*<td\b[^>]*?>\s*(.*?)\s*</td>@i',
+				'emitente_nfe_desde'	=> '@<td\b[^>]*?>\s*Emitente\s*de\s*NFe\s*desde:\s*</td>\s*<td\b[^>]*?>\s*(.*?)\s*</td>@i',
+				'obrigada_nfe'		=> '@<td\b[^>]*?>\s*Obrigada\s*a\s*NF-e\s*em:\s*</td>\s*<td\b[^>]*?>\s*(.*?)\s*</td>@i'							
+				);
 							
 		
 		foreach ($patterns as $c => $p) {
